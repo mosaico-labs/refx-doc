4053b6b97cb55d29e4492e2ed10f65b5ac461254
Francesco Di Corato <dicorato.francesco@gmail.com>
2025-09-17 16:32:53 +0200

    removed blog from config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -46,21 +46,6 @@ const config: Config = {
           routeBasePath: '/', // Set this to '/'
           // Please change this to your repo.
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
         theme: {
           customCss: './src/css/custom.css',
         },
